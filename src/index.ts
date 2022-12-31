446e940ed076fc11965d19b527d1c15e9b7a72f3
sadick254 <sadickjunior@gmail.com>
2023-01-01 00:53:17 +0300

    Transform nameof to string literal.
    This enable us to tranform any occurance of `nameof<Type>()` to a string
    literal during compile time.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,40 @@
 import ts from 'typescript'
 
+
 export const before = (options?: any, program?: ts.Program) => {
     return (context: ts.TransformationContext) => {
         return (sourceFile: ts.SourceFile) => {
-            return sourceFile;
+
+            const visitor = (node: ts.Node): ts.Node => {
+
+                if(ts.isCallExpression(node)) {
+                    let expressionText: string = '';
+
+                    try {
+                        expressionText = node.expression.getText();
+                    } catch {}
+
+                    if (expressionText === 'nameof' && node.typeArguments && node.typeArguments.length === 1) {
+                        let typeName: string = '';
+                        if (ts.isTypeReferenceNode(node.typeArguments[0])) {
+                            typeName = node.typeArguments[0].typeName.getText();
+                        } else if (ts.isArrayTypeNode(node.typeArguments[0])) {
+                            typeName = `${node.typeArguments[0].elementType.getText()}[]`
+                        }
+
+                        if (!!typeName) {
+                            return ts.factory.createStringLiteral(typeName);
+                        }
+                    }
+
+                } else if (ts.isImportDeclaration(node) && node.moduleSpecifier.getText().includes('nameof-ts-transform')) {
+                    return undefined as unknown as ts.Node;
+                }
+
+                return ts.visitEachChild(node, visitor, context);
+            }
+
+            return ts.visitNode(sourceFile, visitor);
         }
     }
 }
