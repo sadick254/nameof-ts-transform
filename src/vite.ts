5d60709527535554a15e76894479fdc79b71938e
sadick254 <sadickjunior@gmail.com>
2023-01-20 11:23:47 +0300

    Enforce nameof to be run pre build

--- src/vite.ts
+++ src/vite.ts
@@ -3,6 +3,7 @@ import { manual } from './manual'
 export function viteNameofPlugin() {
     return {
         name: 'nameof-ts-transform',
+        enforce: 'pre',
         transform(src: string) {
             return {
                 code:  manual(src)
